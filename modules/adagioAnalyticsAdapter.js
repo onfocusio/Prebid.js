4f6a94dbb255900f9e38ceee2ee369b60f96f928
Olivier <olivier.sazos@gmail.com>
2019-01-10 18:24:52 +0100

    Change adagioAnalyticsAdapter to "endpoint" type
    # Conflicts:
    #	modules/adagioAnalyticsAdapter.js

--- modules/adagioAnalyticsAdapter.js
+++ modules/adagioAnalyticsAdapter.js
@@ -4,15 +4,24 @@
 
 import adapter from 'src/AnalyticsAdapter';
 import adaptermanager from 'src/adaptermanager';
+import CONSTANTS from 'src/constants.json';
 
-// This config makes Prebid.js call this function on each event:
-//   `window['AdagioPrebidAnalytics']('on', eventType, args)`
-// If it is missing, then Prebid.js will immediately log an error,
-// instead of queueing the events until the function appears.
-var adagioAdapter = adapter({
-  global: 'AdagioPrebidAnalytics',
-  handler: 'on',
-  analyticsType: 'bundle'
+const emptyUrl = '';
+const analyticsType = 'endpoint';
+
+window.top.adagioAdapterQueue = window.top.adagioAdapterQueue || [];
+
+const events = Object.values(CONSTANTS.EVENTS);
+
+let adagioAdapter = Object.assign(adapter({ emptyUrl, analyticsType }), {
+  track({ eventType, args }) {
+    if (typeof args !== 'undefined' && events.indexOf(eventType) !== -1) {
+      window.top.adagioAdapterQueue.push({
+        eventType: eventType,
+        args: args
+      });
+    }
+  }
 });
 
 adaptermanager.registerAnalyticsAdapter({
