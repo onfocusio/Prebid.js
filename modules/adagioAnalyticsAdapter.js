bc6ad06d4091206b17145f0f5fcd2cc9916db393
Olivier <olivier.sazos@gmail.com>
2019-01-10 18:24:52 +0100

    Change adagioAnalyticsAdapter to "endpoint" type

--- modules/adagioAnalyticsAdapter.js
+++ modules/adagioAnalyticsAdapter.js
@@ -3,74 +3,20 @@
  */
 
 import adapter from 'src/AnalyticsAdapter';
-import adapterManager from 'src/adapterManager';
+import adaptermanager from 'src/adaptermanager';
 import CONSTANTS from 'src/constants.json';
 
 const emptyUrl = '';
 const analyticsType = 'endpoint';
-const events = Object.keys(CONSTANTS.EVENTS).map(key => CONSTANTS.EVENTS[key]);
 
-const ADSRV_EVENTS = {
-  GPT: {
-    IMPRESSION_VIEWABLE: 'impressionViewable',
-    SLOT_ON_LOAD: 'slotOnLoad',
-    SLOT_RENDER_ENDED: 'slotRenderEnded',
-    SLOT_REQUESTED: 'slotRequested',
-    SLOT_RESPONSE_RECEIVED: 'slotResponseReceived',
-    SLOT_VISIBILITY_CHANGED: 'slotVisibilityChanged',
-  },
-  SAS: {
-    CALL: 'call',
-    CLEAN: 'clean',
-    BEFORE_RENDER: 'beforeRender',
-    CMP_ANSWERED: 'CmpAnswered',
-    CMP_CALLED: 'CmpCalled',
-    LOAD: 'load',
-    NOAD: 'noad',
-    RENDER: 'render',
-    RESET: 'reset'
-  }
-};
+window.top.adagioAdapterQueue = window.top.adagioAdapterQueue || [];
 
-window.top.ADAGIO = window.top.ADAGIO || {};
-window.top.ADAGIO.queue = window.top.ADAGIO.queue || [];
-window.top.ADAGIO.PBAH = window.top.ADAGIO.PBAH || {};
-window.top.ADAGIO.PBAH.q = window.top.ADAGIO.PBAH.q || [];
-
-const adagioEnqueue = function adagioEnqueue(actionName, eventName, event) {
-  window.top.ADAGIO.queue.push({
-    action: actionName,
-    ts: Date.now(),
-    data: Object.assign({}, {eventName}, {event}),
-  });
-}
-
-let googletag = top.googletag || {};
-googletag.cmd = googletag.cmd || [];
-googletag.cmd.push(function() {
-  const gptEvents = Object.keys(ADSRV_EVENTS.GPT).map(key => ADSRV_EVENTS.GPT[key]);
-  gptEvents.forEach(gptEventName => {
-    googletag.pubads().addEventListener(gptEventName, event => {
-      adagioEnqueue('gpt-event', gptEventName, event);
-    });
-  });
-});
-
-let sas = top.sas || {};
-sas.cmd = sas.cmd || [];
-sas.cmd.push(function() {
-  const sasEvents = Object.keys(ADSRV_EVENTS.SAS).map(key => ADSRV_EVENTS.SAS[key]);
-  sasEvents.forEach(sasEventName => {
-    sas.events.on(sasEventName, function(event) {
-      adagioEnqueue('sas-event', sasEventName, event);
-    });
-  });
-});
+const events = Object.values(CONSTANTS.EVENTS);
 
 let adagioAdapter = Object.assign(adapter({ emptyUrl, analyticsType }), {
   track({ eventType, args }) {
     if (typeof args !== 'undefined' && events.indexOf(eventType) !== -1) {
-      window.top.ADAGIO.PBAH.q.push({
+      window.top.adagioAdapterQueue.push({
         eventType: eventType,
         args: args
       });
@@ -78,7 +24,7 @@ let adagioAdapter = Object.assign(adapter({ emptyUrl, analyticsType }), {
   }
 });
 
-adapterManager.registerAnalyticsAdapter({
+adaptermanager.registerAnalyticsAdapter({
   adapter: adagioAdapter,
   code: 'adagio'
 });
