daab3659d4082ade94b8c45df3b59fe5e7ca1f87
Olivier <olivier.sazos@gmail.com>
2019-02-06 18:26:02 +0100

    Add AdServer events tracking in adagioAnalytics

--- modules/adagioAnalyticsAdapter.js
+++ modules/adagioAnalyticsAdapter.js
@@ -8,12 +8,66 @@ import CONSTANTS from 'src/constants.json';
 
 const emptyUrl = '';
 const analyticsType = 'endpoint';
+const events = Object.values(CONSTANTS.EVENTS);
+
+const ADSRV_EVENTS = {
+  GPT: {
+    IMPRESSION_VIEWABLE: 'impressionViewable',
+    SLOT_ON_LOAD: 'slotOnLoad',
+    SLOT_RENDER_ENDED: 'slotRenderEnded',
+    SLOT_REQUESTED: 'slotRequested',
+    SLOT_RESPONSE_RECEIVED: 'slotResponseReceived',
+    SLOT_VISIBILITY_CHANGED: 'slotVisibilityChanged',
+  },
+  SAS: {
+    CALL: 'call',
+    CLEAN: 'clean',
+    BEFORE_RENDER: 'beforeRender',
+    CMP_ANSWERED: 'CmpAnswered',
+    CMP_CALLED: 'CmpCalled',
+    LOAD: 'load',
+    NOAD: 'noad',
+    RENDER: 'render',
+    RESET: 'reset'
+  }
+};
 
 window.top.ADAGIO = window.top.ADAGIO || {};
+window.top.ADAGIO.queue = window.top.ADAGIO.queue || [];
 window.top.ADAGIO.PBAH = window.top.ADAGIO.PBAH || {};
 window.top.ADAGIO.PBAH.q = window.top.ADAGIO.PBAH.q || [];
 
-const events = Object.values(CONSTANTS.EVENTS);
+const adagioEnqueue = function adagioEnqueue(actionName, eventName, event) {
+  window.top.ADAGIO.queue.push({
+    action: actionName,
+    ts: Date.now(),
+    data: Object.assign({}, {eventName}, {event}),
+  });
+}
+
+top.googletag = window.googletag || {};
+top.googletag.cmd = googletag.cmd || [];
+top.googletag.cmd.push(function() {
+  const gptEvents = Object.values(ADSRV_EVENTS.GPT);
+  gptEvents.forEach(gptEventName => {
+    console.log(`enqueue ${gptEventName}`);
+    googletag.pubads().addEventListener(gptEventName, event => {
+      adagioEnqueue('gpt-event', gptEventName, event);
+    });
+  });
+});
+
+top.sas = top.sas || {};
+top.sas.cmd = top.sas.cmd || [];
+
+top.sas.cmd.push(function() {
+  const sasEvents = Object.values(ADSRV_EVENTS.SAS);
+  sasEvents.forEach(sasEventName => {
+    top.sas.events.on(sasEventName, function(event) {
+      adagioEnqueue('sas-event', sasEventName, event);
+    });
+  });
+});
 
 let adagioAdapter = Object.assign(adapter({ emptyUrl, analyticsType }), {
   track({ eventType, args }) {
